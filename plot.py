86511afc5701ec3861ea4f158effed74179da511
effbiae <effbiae@gmail.com>
2026-08-13 18:12:44 +1000

    bar charts work

--- plot.py
+++ plot.py
@@ -1,15 +1,24 @@
-import matplotlib.pyplot as p;import json as j;from functools import *
+import matplotlib.pyplot as p;import json as j;from functools import *;from numpy import *
 orin =lambda x,y:reduce(max,[a in y for a in x])
 andin=lambda x,y:reduce(min,[a in y for a in x])
 c='blue red orange'.split()
 with open("plot.data") as f:d=j.load(f)
-fig,ax=p.subplots()
+t=d["t"];fig,ax=p.subplots()
 if type(d["d"])==list:
  d=d["d"];x,y=d[0],d[1]
  if len(d)==3:z=d[2]
 else:
- if orin(["line","scatter"],d["t"]):globals().update(d["d"])
-if andin("xy",globals()):ax.plot(x,y,color=c[0])
-if 'z' in globals():t=ax.twinx();t.plot(x,z,color=c[1])
-if 'm' in globals():ax.vlines(m,ymin=min(y),ymax=max(y),color=c[2])
+ if t in ["line","scatter"]:globals().update(d["d"])
+if t=="line":
+ if andin("xy",globals()):ax.plot(x,y,color=c[0])
+ if 'z' in globals():t=ax.twinx();t.plot(x,z,color=c[1])
+ if 'm' in globals():ax.vlines(m,ymin=min(y),ymax=max(y),color=c[2])
+if t=="bar":
+ d=d["d"];c=list(d.keys());g=list(d.values())
+ x=arange(len(d));width=1/(len(x)**2)
+ for a in x:ax.bar(x+len(x)*a/2*width,g[a],width,label=c[a])
+ ax.set_xticks(x)
+ ax.set_xticklabels([str(a) for a in x])
+ ax.legend()
+
 p.show()
