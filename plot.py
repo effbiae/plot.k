5cacc9e75c3d86af55e626b05129a11b31ca2107
effbiae <effbiae@gmail.com>
2026-08-14 16:34:00 +1000

    waypoint

--- plot.py
+++ plot.py
@@ -1,22 +1,23 @@
-import matplotlib.pyplot as p;import json as j;from functools import *;from numpy import *
-orin =lambda x,y:reduce(max,[a in y for a in x])
-andin=lambda x,y:reduce(min,[a in y for a in x])
-with open("plot.data") as f:d=j.load(f)
-c=d.get("color",'blue red orange'.split())
-t=d["t"];fig,ax=p.subplots()
+import matplotlib.pyplot as p;import json as j
+with open("plot.data")as f:d=j.load(f)
+c=d.get("color",'blue red orange'.split());t=d["t"];fig,ax=p.subplots()
 if type(d["x"])==list:globals().update(dict(zip("xyz",d["x"])))
 else:
- if t in ["line","scatter"]:globals().update(d["x"])
+ if t in["line","scatter"]:globals().update(d["x"])
 if t=="line":
  if"+"in d["x"]:
-  t=d["x"]["+"];[ax.plot(range(len(t[k])),t[k],label=k) for k in t]
+  t=d["x"]["+"];[ax.plot(range(len(t[k])),t[k],label=k)for k in t];p.legend()
  else:
   ax.plot(x,y,color=c[0])
-  if 'z' in globals():t=ax.twinx();t.plot(x,z,color=c[1])
-  if 'm' in globals():ax.vlines(m,ymin=min(y),ymax=max(y),color=c[2])
+  if'z'in globals():
+   t=ax.twinx();t.plot(x,z,color=c[1]);t.tick_params(axis='y',labelcolor='red')
+  if'm'in globals():ax.vlines(m,ymin=min(y),ymax=max(y),color=c[2],linewidth=1)
 if t=="bar":
- t=d["x"]["+"];n=t[""];del t[""];k=list(t.keys());v=list(t.values())
- x=arange(len(t[k[0]]));width=1/len(x)/1.5;f=len(x)/len(t)*width
+ from numpy import*;t=d["x"]["+"];n=t[""];del t[""];k=list(t.keys());v=list(t.values())
+ x=arange(len(t[k[0]]));width=1/len(x)/1.2;f=len(x)/len(t)*width
  [ax.bar(x+a*f,v[a],width,label=k[a])for a in range(len(t))]
  ax.set_xticks(x+f);ax.set_xticklabels(n);ax.legend()
+def set(x,d):
+ if x in dir(p)and x in d:getattr(p,x)(d[x])
+[set(x,d)for x in 'title xlabel'.split()]
 p.show()
