a15187bb6beb66c8f71ac6f2a45bfedcdb492ad4
effbiae <effbiae@gmail.com>
2026-08-14 13:50:53 +1000

    multiline and bar chart fix

--- plot.py
+++ plot.py
@@ -1,21 +1,22 @@
 import matplotlib.pyplot as p;import json as j;from functools import *;from numpy import *
 orin =lambda x,y:reduce(max,[a in y for a in x])
 andin=lambda x,y:reduce(min,[a in y for a in x])
-c='blue red orange'.split()
 with open("plot.data") as f:d=j.load(f)
+c=d.get("color",'blue red orange'.split())
 t=d["t"];fig,ax=p.subplots()
-if type(d["d"])==list:
- d=d["d"];x,y=d[0],d[1]
- if len(d)==3:z=d[2]
+if type(d["x"])==list:globals().update(dict(zip("xyz",d["x"])))
 else:
- if t in ["line","scatter"]:globals().update(d["d"])
+ if t in ["line","scatter"]:globals().update(d["x"])
 if t=="line":
- if andin("xy",globals()):ax.plot(x,y,color=c[0])
- if 'z' in globals():t=ax.twinx();t.plot(x,z,color=c[1])
- if 'm' in globals():ax.vlines(m,ymin=min(y),ymax=max(y),color=c[2])
+ if"+"in d["x"]:
+  t=d["x"]["+"];[ax.plot(range(len(t[k])),t[k],label=k) for k in t]
+ else:
+  ax.plot(x,y,color=c[0])
+  if 'z' in globals():t=ax.twinx();t.plot(x,z,color=c[1])
+  if 'm' in globals():ax.vlines(m,ymin=min(y),ymax=max(y),color=c[2])
 if t=="bar":
- d=d["d"];n=d[""];del d[""];c=list(d.keys());g=list(d.values())
- x=arange(len(d));width=1/(len(x)**2)
- for a in x:ax.bar(x+len(x)*a/2*width,g[a],width,label=c[a])
+ t=d["x"]["+"];n=t[""];del t[""];k=list(t.keys());v=list(t.values())
+ x=arange(len(t[k[0]]));width=1/(len(x)*2)
+ for a in range(len(t)):ax.bar(x+len(x)*a/2*width,v[a],width,label=k[a])
  ax.set_xticks(x+width*len(x)/2);ax.set_xticklabels(n);ax.legend()
 p.show()
