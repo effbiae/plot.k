064617841879dc09dac1b27c0ac844ffb5d57479
effbiae <effbiae@gmail.com>
2026-08-15 16:45:59 +1000

    will generate doc

--- plot.py
+++ plot.py
@@ -1,8 +1,7 @@
-import matplotlib.pyplot as p;import json as j;from numpy import*
-with open("plot.data")as f:d=j.load(f)
+import matplotlib.pyplot as p;import json
+with open("plot.data")as f:d=json.load(f)
 def b(d,ax):
- g={}
- c=d.get("color",'blue red orange'.split());t=d["t"];
+ g={};c=d.get("color",'blue red orange'.split());t=d["t"];
  if type(d["x"])==list:g.update(dict(zip("xyz",d["x"])))
  else:
   if t in["line","scatter"]:g.update(d["x"])
@@ -15,8 +14,8 @@ def b(d,ax):
     t=ax.twinx();t.plot(g['x'],g['z'],color=c[1]);t.tick_params(axis='y',labelcolor='red')
    if'm'in g:ax.vlines(g['m'],ymin=min(g['y']),ymax=max(g['y']),color=c[2],linewidth=1)
  if t=="bar":
-  t=d["x"]["+"];n=t[""];del t[""];k=list(t.keys());v=list(t.values())
-  x=arange(len(t[k[0]]));width=1/len(x)/1.2;f=len(x)/len(t)*width
+  import numpy as np;t=d["x"]["+"];n=t[""];del t[""];k=list(t.keys());v=list(t.values())
+  x=np.arange(len(t[k[0]]));width=1/len(x)/1.2;f=len(x)/len(t)*width
   [ax.bar(x+a*f,v[a],width,label=k[a])for a in range(len(t))]
   ax.set_xticks(x+f);ax.set_xticklabels(n);ax.legend()
 if'rc'in d:
@@ -32,4 +31,5 @@ def set(x,d):
  for y in[p,fig]:
   if x in dir(y)and x in d:getattr(y,x)(d[x])
 [set(x,d)for x in 'suptitle title xlabel'.split()]
-p.show()
+if "out"in d:p.savefig(d['out'])
+else:p.show()
