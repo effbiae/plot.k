1c4745bb13f97b283e7b9c77729b9e45af6bf711
effbiae <effbiae@gmail.com>
2026-08-17 14:05:29 +1000

    tidy

--- plot.py
+++ plot.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as p;import json
-with open("plot.data")as f:d=json.load(f)
+with open('plot.data')as f:d=json.load(f)
 def b(d,ax):
- g={};c=d.get("color",'blue red orange'.split());t=d["t"];
- if type(d["x"])==list:g.update(dict(zip("xyz",d["x"])))
+ g={};c=d.get('color','blue red orange'.split());t=d['t'];
+ if type(d['x'])==list:g.update(dict(zip('xyz',d['x'])))
  else:
-  if t in["line","scatter"]:g.update(d["x"])
- if t=="line":
-  if"+"in d["x"]:
-   t=d["x"]["+"];[ax.plot(t[''],t[k],label=k)for k in t if k!=''];p.legend()
+  if t in['line','scatter']:g.update(d['x'])
+ if t=='line':
+  if'+'in d['x']:
+   t=d['x']['+'];[ax.plot(t[''],t[k],label=k)for k in t if k!=''];p.legend()
   else:
    ax.plot(g['x'],g['y'],color=c[0])
    if'z'in g:
     t=ax.twinx();t.plot(g['x'],g['z'],color=c[1]);t.tick_params(axis='y',labelcolor='red')
    if'm'in g:ax.vlines(g['m'],ymin=min(g['y']),ymax=max(g['y']),color=c[2],linewidth=1)
- if t=="bar":
-  import numpy as np;t=d["x"]["+"];n=t[""];del t[""];k=list(t.keys());v=list(t.values())
+ if t=='bar':
+  import numpy as np;t=d['x']['+'];n=t[''];del t[''];k=list(t.keys());v=list(t.values())
   x=np.arange(len(t[k[0]]));width=1/len(x)/1.2;f=len(x)/len(t)*width
   [ax.bar(x+a*f,v[a],width,label=k[a])for a in range(len(t))]
   ax.set_xticks(x+f);ax.set_xticklabels(n);ax.legend()
@@ -31,5 +31,5 @@ def set(x,d):
  for y in[p,fig]:
   if x in dir(y)and x in d:getattr(y,x)(d[x])
 [set(x,d)for x in 'suptitle title xlabel'.split()]
-if"out"in d:p.savefig(d['out'])
+if'out'in d:p.savefig(d['out'])
 else:p.show()
